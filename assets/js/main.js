99a1116b173b14e31267633f28642010ca1dbe8c
0xb0b1 <vcente82@gmail.com>
2021-03-01 09:34:43 -0300

    ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -227,18 +227,3 @@ open.addEventListener("click", () => {
 close.addEventListener("click", () => {
     container.classList.remove("open");
 });
-
-// SOCIAL PANEL JS
-const floating_btn = document.querySelector(".floating-btn");
-const close_btn = document.querySelector(".close-btn");
-const social_panel_container = document.querySelector(
-    ".social-panel-container"
-);
-
-floating_btn.addEventListener("click", () => {
-    social_panel_container.classList.toggle("visible");
-});
-
-close_btn.addEventListener("click", () => {
-    social_panel_container.classList.remove("visible");
-});
