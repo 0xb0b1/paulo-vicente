5de017b0f3c7b6980a8e5ae03d1d3a4dd6990602
0xb0b1 <vcente82@gmail.com>
2021-03-01 09:20:58 -0300

    ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,200 +1,244 @@
-const tl = gsap.timeline({ defaults: { ease: "power1.out" } })
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-tl.to(".text", { y: "0%", duration: 1.7, stagger: 0.25 })
-tl.to(".intro", { y: "-100%", duration: 1 }, "-=1")
-tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 })
+tl.to(".text", { y: "0%", duration: 1.7, stagger: 0.25 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
 
 /* MENU SHOW */
-const showMenu = (toggleId, navId) =>{
+const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-          nav = document.getElementById(navId)
+        nav = document.getElementById(navId);
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show')
-        })
+    if (toggle && nav) {
+        toggle.addEventListener("click", () => {
+            nav.classList.toggle("show");
+        });
     }
-}
-showMenu('nav-toggle','nav-menu')
+};
+showMenu("nav-toggle", "nav-menu");
 
 /* ACTIVE AND REMOVE MENU */
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
-function linkAction(){
+function linkAction() {
     /*Active link*/
-    navLink.forEach(n => n.classList.remove('active'))
-    this.classList.add('active')
+    navLink.forEach((n) => n.classList.remove("active"));
+    this.classList.add("active");
 
     /*Remove menu mobile*/
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show')
+    const navMenu = document.getElementById("nav-menu");
+    navMenu.classList.remove("show");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /* SCROLL REVEAL ANIMATION */
 const sr = ScrollReveal({
-    distance: '30px',
+    distance: "30px",
     duration: 1800,
-    reset: true
-})
+    reset: true,
+});
 
 /* = SCROLL HOME = */
-sr.reveal('.home__title',{delay:100, origin:'top'})
-sr.reveal('.button',{delay: 100, origin: 'bottom'})
-sr.reveal('.home__img',{delay: 400, origin: 'bottom'})
-sr.reveal('.home__social-icon',{ interval: 300, origin: 'top'})
-
+sr.reveal(".home__title", { delay: 100, origin: "top" });
+sr.reveal(".button", { delay: 100, origin: "bottom" });
+sr.reveal(".home__img", { delay: 400, origin: "bottom" });
+sr.reveal(".home__social-icon", { interval: 300, origin: "top" });
 
 /* = SCROLL ABOUT = */
-sr.reveal('.buttoncv',{origin:'bottom'})
-sr.reveal('.send__content',{delay: 300, origin: 'left'})
-sr.reveal('.send__img',{delay: 300, origin: 'right'})
+sr.reveal(".buttoncv", { origin: "bottom" });
+sr.reveal(".send__content", { delay: 300, origin: "left" });
+sr.reveal(".send__img", { delay: 300, origin: "right" });
 
 /* = SCROLL SKILLS = */
-sr.reveal('.skills__subtitle',{})
-sr.reveal('.skills__text',{})
-sr.reveal('.about__text',{interval: 200, origin: 'bottom'})
-sr.reveal('.hab__img',{delay: 200, origin: 'top'})
+sr.reveal(".skills__subtitle", {});
+sr.reveal(".skills__text", {});
+sr.reveal(".about__text", { interval: 200, origin: "bottom" });
+sr.reveal(".hab__img", { delay: 200, origin: "top" });
 
 /* = SCROLL WORK = */
-sr.reveal('.work__img',{interval: 150, origin:'top'})
+sr.reveal(".work__img", { interval: 150, origin: "top" });
 
 /* = SCROLL LET'S TALK = */
-sr.reveal('.send__content1',{delay: 200, origin: 'bottom'})
+sr.reveal(".send__content1", { delay: 200, origin: "bottom" });
 
 /* = SCROLL CONTACT = */
-sr.reveal('.contact__input',{interval: 200})
-
+sr.reveal(".contact__input", { interval: 200 });
 
 /* BACK TO TOP BUTTON */
-const backToTopButton = document.querySelector("#back-to-top-btn")
+const backToTopButton = document.querySelector("#back-to-top-btn");
 
-window.addEventListener("scroll", scrollFunction)
+window.addEventListener("scroll", scrollFunction);
 
-window.addEventListener("width", matchMedia) // Attach listener function on state changes
+window.addEventListener("width", matchMedia); // Attach listener function on state changes
 
-
-var x = window.matchMedia("(min-width: 768px)")
-scrollFunction(x) // Call listener function at run time
+var x = window.matchMedia("(min-width: 768px)");
+scrollFunction(x); // Call listener function at run time
 
 function scrollFunction() {
-    if (window.pageYOffset > 300) { // Show backToTopButton
-        if (x.matches){
+    if (window.pageYOffset > 300) {
+        // Show backToTopButton
+        if (x.matches) {
             if (!backToTopButton.classList.contains("btnEntrance")) {
-                backToTopButton.classList.remove("btnExit")
-                backToTopButton.classList.add("btnEntrance")
-                backToTopButton.style.display = "block"
+                backToTopButton.classList.remove("btnExit");
+                backToTopButton.classList.add("btnEntrance");
+                backToTopButton.style.display = "block";
             }
         }
-    }
-
-    else { // Hide backToTopButton
-        if(backToTopButton.classList.contains("btnEntrance")) {
-            backToTopButton.classList.remove("btnEntrance")
-            backToTopButton.classList.add("btnExit")
-            setTimeout(function() {
-                backToTopButton.style.display = "none"
-            }, 250)
+    } else {
+        // Hide backToTopButton
+        if (backToTopButton.classList.contains("btnEntrance")) {
+            backToTopButton.classList.remove("btnEntrance");
+            backToTopButton.classList.add("btnExit");
+            setTimeout(function () {
+                backToTopButton.style.display = "none";
+            }, 250);
         }
     }
 }
 
-backToTopButton.addEventListener("click", smoothScrollBackToTop)
+backToTopButton.addEventListener("click", smoothScrollBackToTop);
 
 // function backToTop() {
 //   window.scrollTo(0, 0)
 // }
 
 function smoothScrollBackToTop() {
-    const targetPosition = 0
-    const startPosition = window.pageYOffset
-    const distance = targetPosition - startPosition
-    const duration = 750
-    let start = null
+    const targetPosition = 0;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    const duration = 750;
+    let start = null;
 
-    window.requestAnimationFrame(step)
+    window.requestAnimationFrame(step);
 
     function step(timestamp) {
-        if (!start) start = timestamp
-        const progress = timestamp - start
-        window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration))
-        if (progress < duration) window.requestAnimationFrame(step)
+        if (!start) start = timestamp;
+        const progress = timestamp - start;
+        window.scrollTo(
+            0,
+            easeInOutCubic(progress, startPosition, distance, duration)
+        );
+        if (progress < duration) window.requestAnimationFrame(step);
     }
 }
 
 function easeInOutCubic(t, b, c, d) {
-    t /= d/2
-    if (t < 1) return c/2*t*t*t + b
-    t -= 2
-    return c/2*(t*t*t + 2) + b
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t * t + b;
+    t -= 2;
+    return (c / 2) * (t * t * t + 2) + b;
 }
 
-
 /* DARK-LIGHT THEME */
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'bxs-toggle-right'
-const themeImage = document.getElementById('logo')
-const imgTheme = 'dark-img'
-const themeImage1 = document.getElementById('navlogo')
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "bxs-toggle-right";
+const themeImage = document.getElementById("logo");
+const imgTheme = "dark-img";
+const themeImage1 = document.getElementById("navlogo");
 
 // Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
-const selectedImg = localStorage.getItem('selected-img')
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+const selectedImg = localStorage.getItem("selected-img");
 
 // We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentTheme = () =>
+    document.body.classList.contains(darkTheme) ? "dark" : "light";
 
-const getCurrentImg = () => themeImage.classList.contains(imgTheme) ? 'dark-img' : 'light-img'
+const getCurrentImg = () =>
+    themeImage.classList.contains(imgTheme) ? "dark-img" : "light-img";
 
-const getCurrentImg1 = () => themeImage1.classList.contains(imgTheme) ? 'dark-img' : 'light-img'
+const getCurrentImg1 = () =>
+    themeImage1.classList.contains(imgTheme) ? "dark-img" : "light-img";
 
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-toggle-left' : 'bxs-toggle-right'
+const getCurrentIcon = () =>
+    themeButton.classList.contains(iconTheme)
+        ? "bx-toggle-left"
+        : "bxs-toggle-right";
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
     // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+        darkTheme
+    );
 
-    themeImage.classList[selectedImg === 'dark-img' ? 'add' : 'remove'](imgTheme)
+    themeImage.classList[selectedImg === "dark-img" ? "add" : "remove"](
+        imgTheme
+    );
 
-    themeImage1.classList[selectedImg === 'dark-img' ? 'add' : 'remove'](imgTheme)
+    themeImage1.classList[selectedImg === "dark-img" ? "add" : "remove"](
+        imgTheme
+    );
 
-    themeButton.classList[selectedIcon === 'bx-toggle-left' ? 'add' : 'remove'](iconTheme)
+    themeButton.classList[selectedIcon === "bx-toggle-left" ? "add" : "remove"](
+        iconTheme
+    );
 }
 
 // Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
+themeButton.addEventListener("click", () => {
     // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    themeImage.classList.toggle(imgTheme)
-    themeImage1.classList.toggle(imgTheme)
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+    themeImage.classList.toggle(imgTheme);
+    themeImage1.classList.toggle(imgTheme);
 
     // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-    localStorage.setItem('selected-img' , getCurrentImg())
-})
+    localStorage.setItem("selected-theme", getCurrentTheme());
+    localStorage.setItem("selected-icon", getCurrentIcon());
+    localStorage.setItem("selected-img", getCurrentImg());
+});
 
 /* scroll section active link */
-const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll("section[id]");
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+function scrollActive() {
+    const scrollY = window.pageYOffset;
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50
-        sectionId = current.getAttribute('id')
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute("id");
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document
+                .querySelector(".nav__menu a[href*=" + sectionId + "]")
+                .classList.add("active");
+        } else {
+            document
+                .querySelector(".nav__menu a[href*=" + sectionId + "]")
+                .classList.remove("active");
         }
-    })
+    });
 }
-window.addEventListener('scroll', scrollActive)
+window.addEventListener("scroll", scrollActive);
+
+// social links
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const container = document.getElementById("container");
+
+open.addEventListener("click", () => {
+    container.classList.add("open");
+});
+
+close.addEventListener("click", () => {
+    container.classList.remove("open");
+});
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector(".floating-btn");
+const close_btn = document.querySelector(".close-btn");
+const social_panel_container = document.querySelector(
+    ".social-panel-container"
+);
+
+floating_btn.addEventListener("click", () => {
+    social_panel_container.classList.toggle("visible");
+});
+
+close_btn.addEventListener("click", () => {
+    social_panel_container.classList.remove("visible");
+});
